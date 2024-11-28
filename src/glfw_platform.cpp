48cb2aeeb34d3969c6b970d9a8b869793c704d61
vadym <coda.flow@gmail.com>
2024-11-28 17:20:27 +0200

    add assimp

--- src/glfw_platform.cpp
+++ src/glfw_platform.cpp
@@ -2,13 +2,18 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <assimp/cimport.h>
 #include <glm/glm.hpp>
 
 int main() {
     glfwSetErrorCallback([](int error, const char *description) { fprintf(stderr, "Error: %s\n", description); });
 
-    if (!glfwInit()) exit(EXIT_FAILURE);
+    const aiScene *scene = aiImportFile("data/rubber_duck/scene.gltf", aiProcess_Triangulate);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -21,12 +26,16 @@ int main() {
     }
 
     glfwSetKeyCallback(window, [](GLFWwindow *window, int key, int scancode, int action, int mods) {
-        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) glfwSetWindowShouldClose(window, GLFW_TRUE);
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
     });
 
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     glfwSwapInterval(1);
-
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        // Keep running
+    }
     return 0;
 }
